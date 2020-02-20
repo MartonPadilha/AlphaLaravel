29c81ee4df1a595fd1f1772c08d186b30b49db47
MartonPadilha <martonpad@gmail.com>
2020-02-20 19:54:48 -0300

    Melhorias nas views

--- resources/views/admin/user/listUsersAll.blade.php
+++ resources/views/admin/user/listUsersAll.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Usuários</title>
-</head>
-<body>
 @extends('admin.master.layout')
 @section('content')
     <div class="container">
@@ -70,5 +61,3 @@
         </table>
     </div>
 @endsection
-</body>
-</html>
