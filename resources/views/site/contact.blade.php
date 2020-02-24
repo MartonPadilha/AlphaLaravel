9e98c0dc3f89d13ff8d9564ecb94217feb39a43e
MartonPadilha <martonpad@gmail.com>
2020-02-24 17:53:41 -0300

    Ajuste do mapa da página de contato

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -1,8 +1,5 @@
 @extends('site.master.layout')
 @section('content')
-<script async defer
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLegfBqUcQLkSTWYyUEUqnIAWPnPhn-5c&callback=initMap">
-</script>
 
 <div class="jumbotron jumbotron-fluid" style="height: 40vh; background-image: url('{{asset('assets/images/1280x400/instalacoes.jpg')}}')">
 	<div class="container">
@@ -13,11 +10,10 @@
         <p class="agileits_w3layouts_para w3_agile_para">Nossa empresa está de portas abertas para recebê-lo! Conheça nossa estrutura e nossos processos e comprove que nossos produtos atendem às mais rígidas normas internacionais de qualidade.</p>      
     </div>
       
-
 <div class="agileits_w3layouts_mail_grids">	
     <div class="col-md-7 w3l_mail_left">
         <div id="map">
-            {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1755.1124465738596!2d-53.89313854177375!3d-28.382274635832317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94fc32959b640d71%3A0x5a92ef863c677b9c!2sR.%20Augusto%20Fengler%2C%20427%20-%20Jardim%2C%20Iju%C3%AD%20-%20RS%2C%2098700-000!5e0!3m2!1spt-BR!2sbr!4v1579203365488!5m2!1spt-BR!2sbr"  frameborder="0" style="border:0;" allowfullscreen=""></iframe> --}}
+            <iframe width="100%" height="100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1755.1124465738596!2d-53.89313854177375!3d-28.382274635832317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94fc32959b640d71%3A0x5a92ef863c677b9c!2sR.%20Augusto%20Fengler%2C%20427%20-%20Jardim%2C%20Iju%C3%AD%20-%20RS%2C%2098700-000!5e0!3m2!1spt-BR!2sbr!4v1579203365488!5m2!1spt-BR!2sbr"  frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>
     </div>
     <div class="col-md-5 w3l_mail_right">
@@ -70,19 +66,7 @@
     </div>
 </div>
 <!-- //mail -->
-<!-- map -->
-<script type="text/javascript">
-    // When the window has finished loading create our google map below
-    google.maps.event.addDomListener(window, 'load', init);
 
-    function init() {
-        map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -28.3823946, lng: -53.8906462},
-        zoom: 16
-        });
-        }
-</script>
-<!-- //map -->
 <script src="js/classie.js"></script>
 <script>
     (function() {
